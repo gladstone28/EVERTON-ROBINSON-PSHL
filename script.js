0d17c8f4e17f954cf1b3c58b97b4c0d4ecbf2bd4
Gladstone Rose <gladstonerose@hotmail.com>
2024-02-12 07:53:55 -0500

    added new graphics of furniture pieces

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     const jobs = [{
             client: "Client 1",
             date: "2024-03-01",
-            image: "PROFESSIONAL-SERVICES-HUB-FRONT.jpeg",
-            description: "Job overview for Client 1..."
+            image: "furniture-1.webp",
+            description: "This living room set was delivered to Mary campbell on 01 March 2024. More Job overview for Client 1..."
         },
         {
             client: "Client 2",
             date: "2024-03-02",
-            image: "PROFESSIONAL-SERVICES-HUB-BACK.jpeg",
-            description: "Job overview for Client 2..."
+            image: "furniture-2.webp",
+            description: "This Bedroom set was deliverd to John Brown on 02 March 2024. More Job overview for Client 2..."
         },
         {
             client: "Client 3",
             date: "2024-03-03",
-            image: "professional-services-scan.jpg",
-            description: "Job overview for Client 3..."
+            image: "furniture-3.webp",
+            description: "This Kitchen set was delivered to Gladstone Green on 03 March 2024. More Job overview for Client 3..."
         },
         {
             client: "Client 4",
             date: "2024-03-04",
-            image: "PROFESSIONAL-SERVICES-HUB-FRONT.jpeg",
-            description: "Job overview for Client 4..."
+            image: "furniture-4.webp",
+            description: "This Patio Set was delivered to Howard Palmer omm 04 March 2024. More Job overview for Client 4..."
         },
         {
             client: "Client 5",
             date: "2024-03-05",
-            image: "PROFESSIONAL-SERVICES-HUB-FRONT.jpeg",
-            description: "Job overview for Client 5..."
+            image: "furniture-5.webp",
+            description: "This Dining Room set was delivered to Millicent Brown on 05 March 2024. More Job overview for Client 5..."
         },
         // Add more jobs as needed
     ];
